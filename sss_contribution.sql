f81bbb1fc400593acb794f9a8fd72c535f5dbfe4
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-11-06 11:00:35 +0800

    Update sss_contribution.sql

--- sss_contribution.sql
+++ sss_contribution.sql
@@ -4,6 +4,15 @@ SELECT
     COALESCE(SUM(sd.amount), 0) + COALESCE(SUM(sder.amount), 0) AS `SS:Currency:150`,
     COALESCE(SUM(sdec.amount), 0) AS `EC:Currency:150`,
     COALESCE(SUM(sd.amount), 0) + COALESCE(SUM(sder.amount), 0) + COALESCE(SUM(sdec.amount), 0) AS `Total Contributions:Currency:150`
+
+    For HHI
+    COALESCE(SUM(sd.amount), 0) AS `Employee Share:Currency:150`,
+    COALESCE(SUM(sder.amount), 0) AS `Employer Share:Currency:150`,
+    COALESCE(SUM(sdec.amount), 0) AS `ECola:Currency:150`,
+    COALESCE(SUM(sd.amount), 0) + COALESCE(SUM(sder.amount), 0) + COALESCE(SUM(sdec.amount), 0) AS `Total Contributions:Currency:150`
+
+
+
 FROM
     `tabEmployee` emp
 LEFT JOIN
